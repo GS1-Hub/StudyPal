e273adc19666702b3bb723c82eedcdbcbcd27758
GS1-HUB <2241539@my.ipleiria.pt>
2026-04-08 21:38:13 +0100

    add dates for start task and end

---
+++ database/migrations/2026_04_08_202857_add_started_at_completed_at_to_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('started_at')->nullable()->after('due_date');
+            $table->timestamp('completed_at')->nullable()->after('started_at');
+            $table->dropColumn('completed'); // remove a coluna antiga
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn(['started_at', 'completed_at']);
+            $table->boolean('completed')->default(false); // restaura se fizeres rollback
+        });
+    }
+};
